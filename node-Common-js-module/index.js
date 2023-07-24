4debecbb13a4cc21e82f4d0b0ccf0443be6b5207
nzend <115723110+nzend@users.noreply.github.com>
2023-07-24 17:31:28 +0300

    init

---
+++ node-Common-js-module/index.js
@@ -0,0 +1,12 @@
+const obj = require("./users");
+console.log(obj);
+
+const {admins} = require("./users");
+console.log(admins);
+
+// const {getCurrentMonth} = require("./date");
+// const currentMonth = getCurrentMonth();
+// console.log(`Now ${currentMonth} month`);
+
+const currentMonth = require("./date").getCurrentMonth();
+console.log(`Now ${currentMonth} month`);
