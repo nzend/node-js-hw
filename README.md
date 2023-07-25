de1217ff2ae2b4c98010bd12293f651f56a7ee97
nzend <115723110+nzend@users.noreply.github.com>
2023-07-25 15:52:47 +0300

    Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # node-js-hw
+Get all contacts https://ibb.co/B6LnB8z
+
+Get a contact by id https://ibb.co/QNGGLqs
+
+Add a new contact https://ibb.co/hsGfzqj
+
+Remove a contact from a list https://ibb.co/zhbhdpy
--- index.js
+++ index.js
@@ -5,20 +5,20 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await contacts.listContacts();
-          return console.log(allContacts);
-      
+      return console.table (allContacts);
+
     case "get":
       const getContact = await contacts.getContactById(id);
-          return console.log(getContact);
-      
+      return console.log(getContact);
+
     case "add":
       const newContact = await contacts.addContact({ name, email, phone });
-          return console.log(newContact);
-      
+      return console.log(newContact);
+
     case "remove":
       const deletedContact = await contacts.removeContact(id);
-          return console.log(deletedContact);
-      
+      return console.log(deletedContact);
+
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
