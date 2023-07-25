e1ceef013dfe798f56a5edece6e0bc72e131d668
nzend <115723110+nzend@users.noreply.github.com>
2023-07-25 12:25:44 +0300

    add process

---
+++ contacts.js
@@ -0,0 +1,44 @@
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [contact] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return contact;
+}
+
+async function addContact(data) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-console.log("Hello");
+const { program } = require("commander");
+const contacts = require("./contacts");
 
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+          return console.log(allContacts);
+      
+    case "get":
+      const getContact = await contacts.getContactById(id);
+          return console.log(getContact);
+      
+    case "add":
+      const newContact = await contacts.addContact({ name, email, phone });
+          return console.log(newContact);
+      
+    case "remove":
+      const deletedContact = await contacts.removeContact(id);
+          return console.log(deletedContact);
+      
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
 
+program
+  .option("-a, --action, <type>")
+  .option("-i, --id, <type>")
+  .option("-n, --name, <type>")
+  .option("-e, --email, <type>")
+  .option("-p, --phone, <type>");
+
+program.parse();
+
+const options = program.opts();
+
+invokeAction(options);
